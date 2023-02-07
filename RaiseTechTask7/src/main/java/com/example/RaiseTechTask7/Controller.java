87d3de3ea20616d10408f25e5429c6ae776b877d
KodaiMeboso-Java <muxiguangda@gmail.com>
2023-02-07 21:24:14 +0900

    first commit

---
+++ RaiseTechTask7/src/main/java/com/example/RaiseTechTask7/Controller.java
@@ -0,0 +1,21 @@
+package com.example.RaiseTechTask7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Controller {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Controller.class, args);
+    }
+}
+
